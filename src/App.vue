9f4768a6ab24ba49537e38f8dc60dcef30dce90c
ilchovski98 <ilchovski67bg@gmail.com>
2020-06-10 23:15:16 +0300

    connecting api and adding cards

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
-    <card></card>
-    <fetcher :url="url1"></fetcher>
+    
+    <fetcher class="space" :url="url1">
+      <template #default="{ apiData }">
+        <card>{{ apiData.name }}</card>
+      </template>
+    </fetcher>
+
+    <fetcher class="space" :url="url2">
+      <template #default="{ apiData }">
+        <card>{{ apiData.name }}</card>
+      </template>
+    </fetcher>
+
+    <fetcher class="space" :url="url3">
+      <template #default="{ apiData }">
+        <card>{{ apiData.name }}</card>
+      </template>
+    </fetcher>
+
+    <fetcher class="space" :url="url4">
+      <template #default="{ apiData }">
+        <card>{{ apiData.name }}</card>
+      </template>
+    </fetcher>
   </div>
 </template>
 
 <script>
 import Card from './components/card'
 import Fetcher from './components/fetcher'
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 export default {
   name: 'App',
@@ -34,5 +58,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.space {
+  margin: 20px;
 }
 </style>
--- src/components/fetcher/index.vue
+++ src/components/fetcher/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-
+      <slot :apiData="apiData"></slot>
   </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
     name: 'Fetcher',
     props: ['url'],
-    
+    data() {
+        return {
+            apiData: ''
+        }
+    },
+    mounted() {
+        axios.get(this.url).then(response => {
+            this.apiData = response.data
+        })
+    }
 }
 </script>
 
